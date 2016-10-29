bbf514dd56cd67551ee4215b7cf6a46c6807d1b3
Serden Bulut <serden.bulut@evatro.com>
2016-10-29 15:43:35 +0300

    Denmee
    Deneme

--- DealerNotificationSystem/Global.asax.cs
+++ DealerNotificationSystem/Global.asax.cs
@@ -12,6 +12,7 @@ namespace DealerNotificationSystem
     {
         protected void Application_Start()
         {
+// Denmee
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
