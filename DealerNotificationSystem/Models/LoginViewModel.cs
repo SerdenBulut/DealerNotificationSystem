21d577649b2e6f2e8b7eaf3f8ea38972ce039f72
serdal topkaya <serdal.topkaya@gmail.com>
2016-10-30 20:28:25 +0200

    login page view added

---
+++ DealerNotificationSystem/Models/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DealerNotificationSystem.Models
+{
+    public sealed class LoginViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Kullanıcı adı zorunlu alandır.")]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Şifre zorunlu alandır.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
