13f5f6bcea1e1ec1d99c0e9adca4cbe74e683073
serdal topkaya <serdal.topkaya@gmail.com>
2016-11-12 21:28:52 +0200

    sistem c nin altına taşındı

--- DNS.WebApi/Global.asax.cs
+++ DNS.WebApi/Global.asax.cs
@@ -17,7 +17,6 @@ namespace DNS.WebApi
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
