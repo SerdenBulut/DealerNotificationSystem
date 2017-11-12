05790d8f11d4fcd6a0a8a78a823fee7376b5e38c
Serden <serdenbulut@gmail.com>
2017-11-12 15:04:59 +0300

    Deneme
    Deneme

--- DealerNotificationSystem/Controllers/LoginController.cs
+++ DealerNotificationSystem/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Mvc;
+using Dns.DbContext;
+using System.Web.Security;
 
 namespace DNS.Web.Controllers
 {
@@ -10,9 +12,41 @@ namespace DNS.Web.Controllers
             return View();
         }
 
-        public ActionResult LoginOperation()
+        [HttpPost]
+        public ActionResult Login(User model, string returnUrl)
         {
-            return Redirect("/Home/Index");
+            // Lets first check if the Model is valid or not
+            if (ModelState.IsValid)
+            {
+                DNSEntities entities = new DNSEntities();
+                    string username = model.UserName;
+                    string password = model.Password;
+
+                    bool userValid = entities.Users.Any(user => user.username == username && user.password == password);
+
+                    // User found in the database
+                    if (userValid)
+                    {
+
+                        FormsAuthentication.SetAuthCookie(username, false);
+                        if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                            && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                        {
+                            return Redirect(returnUrl);
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                    }
+                
+            }
+
+            return View(model);
         }
     }
 }
